4e14203fc743b8ec6b40d66e2f3df18d38794739
Rafsan Jany <59754354+RafsanJany-44@users.noreply.github.com>
2022-03-31 01:41:13 +0600

    up

--- b_tree_1.cpp
+++ b_tree_1.cpp
@@ -1,6 +1,37 @@
 #include<iostream>
 using namespace std;
 
+struct Node{
+    int data;
+    int data2;
+    int data3;
+    Node *next1;
+    Node *next2;
+    Node *next3;
+};
+
+class LinkedList{
+private:
+Node *head;   // it has 2 data and 3 linked list
+
+public:
+
+LinkedList(){
+    head=NULL;
+}
+
+ void addNodeAtFront(int data,int data2,int data3){
+        Node *n = new Node;
+        n->data = data;
+        n->data2 = data2;
+        n->data3 = data3;
+        n->next = head;
+        head = n;
+    }
+
+
+};
+
 int main(){
 
     int n;
