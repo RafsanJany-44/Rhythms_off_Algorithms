cce406d659d5af7b9f6934b7894e7def46ccd425
Rafsan Jany <59754354+RafsanJany-44@users.noreply.github.com>
2022-04-01 00:34:31 +0600

    up

---
+++ Randomize_Algo.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int Partition(int A[],int last_index,int pivot){
+    int last_element=A[last_index];
+    int index=pivot-1;
+    for(int j=pivot;j<=last_index-1;j++){
+        if(A[j]<=last_element)
+        {
+            index=index+1;
+            swap(A[j],A[index]);
+        }
+    }
+    swap(A[index+1],A[last_index]);
+    for (int i = 0; i <=last_index; i++) {
+        cout << A[i] << "  ";
+    }
+    cout << endl;
+    return index+1;
+}
+
+int RandomizeAlgo(int A[],int start,int end,int index){
+    if(start==end){
+        return A[index];
+    }
+
+    int mid=Partition(A,start,end);
+    int rank=mid-start+1;
+    
+    
+}
+int  main(){}
+
+
+
+
+
+
+
+
+
+
+
